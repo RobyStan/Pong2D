881698954d817b05bb384aa9d7cd1e29ba9e1a11
RobyStan <robert-ionut.stan@s.unibuc.ro>
2023-10-25 23:21:58 +0300

    Tema 1

--- main.cpp
+++ main.cpp
@@ -1,116 +1,284 @@
-#include <rlutil.h>
-#include <digestpp.hpp>
-#include <random.hpp>
-#include <csv.hpp>
-#include <date.h>
-
 #include <iostream>
-#include <cctype>
-#include <chrono>
-#include <thread>
+#include "rlutil.h"
 
-std::string make_salt() {
-    // important este ca salt-ul să fie unic, nu contează că nu este aleator
-    // se stochează ca text clar, lângă parola hashed
-    static uint64_t nr = 1u;
-    std::string salt;
-    auto bytes = static_cast<unsigned char*>(static_cast<void*>(&nr));
-    for(unsigned i = 0; i < 16; i++) {
-        salt += bytes[i%8];
+enum GameStatus
+{
+    START = 1,
+    QUIT = 2
+};
+
+class Paddle
+{
+private:
+    int x, y;
+    int width, height;
+public:
+    Paddle(int startX, int startY, int paddleWidth, int paddleHeight):
+            x(startX), y(startY),
+            width(paddleWidth), height(paddleHeight) {}
+
+    Paddle(const Paddle& other) : x(other.x), y(other.y), width(other.width), height(other.height) {}
+
+    Paddle& operator=(const Paddle& other)
+    {
+        this->x = other.x;
+        this->y = other.y;
+        this->width = other.width;
+        this->height = other.height;
+        return *this;
     }
-    ++nr;
-    return salt;
+
+    ~Paddle() {}
+
+    friend std::ostream& operator<<(std::ostream& os, const Paddle& paddle);
+
+    int getX() const
+    {
+        return x;
+    }
+
+    int getY() const
+    {
+        return y;
+    }
+
+    int getWidth() const
+    {
+        return width;
+    }
+
+    int getHeight() const
+    {
+        return height;
+    }
+
+    void moveUp()
+    {
+        if (y > 0)
+        {
+            y--;
+        }
+    }
+
+    void moveDown(int screenHeight)
+    {
+        if (y + height < screenHeight)
+        {
+            y++;
+        }
+    }
+};
+
+class Ball
+{
+private:
+    int x, y;
+    double xSpeed, ySpeed;
+public:
+    Ball(int startX, int startY)
+            : x(startX), y(startY), xSpeed(.1), ySpeed(1) {}
+
+    int getX() const
+    {
+        return x;
+    }
+    int getY() const
+    {
+        return y;
+    }
+
+    void update()
+    {
+        x = x + xSpeed;
+        y = y + ySpeed;
+    }
+
+    void reverseX()
+    {
+        xSpeed = -xSpeed;
+    }
+
+    void reverseY()
+    {
+        ySpeed = -ySpeed;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Ball& ball);
+};
+
+class Game
+{
+private:
+    int screenWidth, screenHeight;
+    Paddle player1, player2;
+    Ball ball;
+    HANDLE hConsole;
+public:
+    Game(int height, int width) :
+            screenHeight(height), screenWidth(width),
+            player1(1, height / 2 - 3, 1, 6),
+            player2(width - 2, height / 2 - 3, 1, 6),
+            ball(width / 2, height / 2),
+            hConsole(GetStdHandle(STD_OUTPUT_HANDLE)) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const Game& pingPongGame);
+
+    void run()
+    {
+        while (true)
+        {
+            if (kbhit())
+            {
+                if (GetAsyncKeyState('W'))
+                {
+                    player1.moveUp();
+                }
+                if (GetAsyncKeyState('S'))
+                {
+                    player1.moveDown(screenHeight);
+                }
+                if (GetAsyncKeyState(VK_UP))
+                {
+                    player2.moveUp();
+                }
+                if (GetAsyncKeyState(VK_DOWN))
+                {
+                    player2.moveDown(screenHeight);
+                }
+                if(GetAsyncKeyState('Q'))
+                {
+                    break;
+                }
+            }
+
+            ball.update();
+
+            if (ball.getY() == 0 || ball.getY() == screenHeight - 1 )
+            {
+                ball.reverseY();
+            }
+
+            if (ball.getX() == player1.getX() + 1 && ball.getY() >= player1.getY() && ball.getY() < player1.getY() + player1.getHeight())
+            {
+                ball.reverseX();
+            }
+
+            if (ball.getX() == player2.getX() - 1 && ball.getY() >= player2.getY() && ball.getY() < player2.getY() + player2.getHeight())
+            {
+                ball.reverseX();
+            }
+
+            if (ball.getX() == 0 || ball.getX() == screenWidth - 1)
+            {
+                ball = Ball(screenWidth / 2, screenHeight / 2);
+                player1 = Paddle(1, screenHeight / 2 - 3, 1, 6);
+                player2 = Paddle(screenWidth - 2, screenHeight / 2 - 3, 1, 6);
+            }
+
+            render();
+        }
+    }
+
+    void render()
+    {
+        COORD coord = {0, 0};
+        SetConsoleCursorPosition(hConsole, coord);
+
+        for (int i = -1; i <= screenHeight ; i++)
+        {
+            for (int j = 0; j < screenWidth; j++)
+            {
+                if (i == -1 || i == screenHeight)
+                {
+                    std::cout << "~";
+                }
+                else if ((j == player1.getX() || j == player1.getX() + player1.getWidth() - 1) && (i>= player1.getY() && i < player1.getY() + player1.getHeight()))
+                {
+                    std::cout << "|";
+                }
+                else if ((j == player2.getX() || j == player2.getX() + player2.getWidth() - 1) && (i >= player2.getY() && i < player2.getY() + player2.getHeight()))
+                {
+                    std::cout << "|";
+                }
+                else if (j == ball.getX() && i == ball.getY())
+                {
+                    std::cout << "O";
+                }
+                else
+                {
+                    std::cout << " ";
+                }
+            }
+            std::cout << std::endl;
+        }
+    }
+};
+
+void instructions()
+{
+    std::cout << "1 - Start a new game"<<std::endl;
+    std::cout << "2 - Stop the game"<<std::endl;
+    std::cout << "Q - To exit while in game "<<std::endl;
+    std::cout << "---How to play---"<<std::endl;
+    std::cout << "Player 1 (left) -> w - go up / s - go down"<<std::endl;
+    std::cout << "Player 2 (right) -> up arrow - go up / down arrow - go down"<<std::endl;
+    std::cout << "Enter option: ";
 }
 
-std::string hash_password(const std::string& plain) {
-    return digestpp::blake2b(512).set_salt(make_salt()).absorb(plain).hexdigest();
+std::ostream& operator<<(std::ostream& os, const Paddle& paddle)
+{
+    os << "Paddle position (x, y): (" << paddle.x << ", " << paddle.y << ")\n";
+    os << "Paddle dimensions (width, height): (" << paddle.width << ", " << paddle.height << ")\n";
+    return os;
 }
 
-int main() {
-    std::cout << "-----------------------------------------------\n";
+std::ostream& operator<<(std::ostream& os, const Ball& ball)
+{
+    os << "Ball position (x, y): (" << ball.x << ", " << ball.y << ")\n";
+    os << "Ball speed(x_speed, y_speed): (" <<ball.xSpeed << ", " << ball.ySpeed << ")\n";
+    return os;
+}
 
-    std::string plain = "temaOOP12345$";
-    std::cout << hash_password(plain) << "\n"
-              << hash_password(plain) << "\n";
+std::ostream& operator<<(std::ostream& os, const Game& game)
+{
+    os << "Screen Dimensions: " << game.screenWidth << "x" << game.screenHeight << "\n";
+    os << "Player 1:\n" << game.player1;
+    os << "Player 2:\n" << game.player2;
+    os << "Ball Position: (" << game.ball.getX() << ", " << game.ball.getY() << ")\n";
+    return os;
+}
 
-    std::cout << "-----------------------------------------------\n";
+int main()
+{
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    rlutil::setConsoleTitle("test");
-    rlutil::saveDefaultColor();
-    rlutil::setColor(rlutil::BLUE);
-    rlutil::cls();
-    int key = rlutil::getkey(); // apel blocant; apelează kbhit și getch
-    switch(std::tolower(key)) {
-    case rlutil::KEY_SPACE:
-        std::cout << "pressed space\n";
-        break;
-    case 'w':
-        std::cout << "pressed w\n";
-        break;
-    case 'a':
-        std::cout << "pressed a\n";
-        break;
-    case 's':
-        std::cout << "pressed s\n";
-        break;
-    case 'd':
-        std::cout << "pressed d\n";
-        break;
-    default:
-        std::cout << "other key (" << key << ")\n";
-        break;
-    }
-    std::cout << "test color text\n";
-    rlutil::resetColor();
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(consoleHandle, &cursorInfo);
+    cursorInfo.bVisible = false;
+    SetConsoleCursorInfo(consoleHandle, &cursorInfo);
 
-    std::cout << "-----------------------------------------------\n";
+    GameStatus status = START;
 
-    using namespace std::chrono_literals;
-    std::cout << "begin sleep\n";
-    std::this_thread::sleep_for(600ms);
-    std::cout << "end sleep\n";
+    while (status == START)
+    {
+        instructions();
+        int choice;
+        std::cin >> choice;
 
-    std::cout << "-----------------------------------------------\n";
-
-    using Random = effolkronium::random_static;
-    // Random::seed(42);
-    std::cout << Random::get(1, 1000) << "\n";
-
-    std::cout << "-----------------------------------------------\n";
-
-    using namespace csv;
-    CSVReader reader{"date.csv"};
-    for (CSVRow& row : reader) {
-        std::cout << "nume: " << row["nume"].get_sv() << "\n";
-        // std::cout << "nume: " << row["nume"].get<>() << "\n";
+        if (choice == 2)
+        {
+            status = QUIT;
+        }
+        else if (choice == 1)
+        {
+            Game pingPongGame(28, 80);
+            pingPongGame.run();
+        }
+        else
+        {
+            std::cout << "Invalid choice. Please choose 1 to start a game or 2 to quit." << "\n\n";
+        }
     }
 
-    std::cout << "-----------------------------------------------\n";
-
-    using namespace std::chrono;
-    using namespace date;
-    using date::sys_days;
-    using date::days;
-    using date::weeks;
-    using date::months;
-    auto d1 = 2022_y/10/01;
-    auto d2 = 2023_y/05/26;
-
-    auto dp1 = sys_days{d1};
-    auto dp2 = sys_days{d2};
-
-    std::cout << "Anul 2022-2023 are "
-              << duration<float, months::period>(dp2 - dp1).count() << " luni"
-              << " sau "
-              << duration<float, weeks::period>(dp2 - dp1).count() << " săptămâni"
-              << " sau "
-              << duration<float, days::period>(dp2 - dp1).count() << " zile"
-              << ", adică "
-              << floor<months>(dp2 - dp1).count() << " luni, "
-              << floor<weeks>(dp2 - dp1 - floor<months>(dp2 - dp1)).count() << " săptămâni, "
-              << floor<days>(dp2 - dp1 - floor<weeks>(dp2 - dp1)).count() - 1 << " zile."
-              << "\n";
-
-    std::cout << "-----------------------------------------------\n";
     return 0;
 }
